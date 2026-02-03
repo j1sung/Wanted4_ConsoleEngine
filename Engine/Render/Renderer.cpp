733b2528d92034417c45f7f71a681fd74292a686
dalssak <dalssagi00@gmail.com>
2026-02-03 14:52:04 +0900

    Renderer의 핵심 로직 구현
    - Clear/Draw/Present 등 렌더링 로직 구현 추가

--- Engine/Render/Renderer.cpp
+++ Engine/Render/Renderer.cpp
@@ -81,7 +81,83 @@ namespace Wanted
 	}
 	void Renderer::Draw() 
 	{
+		// 화면 지우기.
+		Clear();
 
+		// 전제조건: 레벨의 모든 액터가 렌더러에 Submit을 완료.
+		// 렌더큐 순회하면서 프레임 채우기.
+		for (const RenderCommand& command : renderQueue)
+		{
+			// 화면에 그릴 텍스트가 없으면 건너뜀.
+			if (!command.text)
+			{
+				continue;
+			}
+
+			// 세로 기준 화면 벗어났는지 확인.
+			if (command.position.y < 0 || command.position.y >= screenSize.y)
+			{
+				continue;
+			}
+
+			// 화면에 그릴 문자열 길이.
+			const int length = static_cast<int>(strlen(command.text));
+
+			// 안그려도 되면 건너뜀.
+			if (length <= 0)
+			{
+				continue;
+			}
+			
+			// x좌표 기준으로 화면에서 벗어났는지 확인.
+			// 위치는 왼쪽 기준: "abcde"
+			// length가 5인데 실제론 a부터 왼쪽으로 1칸씩 이동할땐 4번이니 1을 뺀다.
+			const int startX = command.position.x;
+			const int endX = command.position.x + length - 1; // 여기서 1을 빼야한다.
+
+			// 가로에서 화면 벗어났는지 확인
+			if (endX < 0 || startX >= screenSize.x)
+			{
+				continue;
+			}
+
+			// 시작 위치.
+			// ab|cde 이렇게 |가 화면 끝이고 a, b가 벗어나서 음수일때 0으로 초기화해서 화면 끝지점 벗어나면 0으로 초기화
+			const int visibleStart = startX < 0 ? 0 : startX;
+			const int visibleEnd = endX >= screenSize.x ? screenSize.x - 1 : endX;
+
+			// 문자열 설정.
+			for (int x = visibleStart; x <= visibleEnd; ++x)
+			{
+				// 문자열 안의 문자 인덱스.
+				const int sourceIndex = x - startX;
+
+				// 프레임 (2차원 문자 배열) 인덱스.
+				const int index = (command.position.y * screenSize.x) + x;
+
+				// 그리기 우선순위 비교. - 같은건 비교 안했음
+				if (frame->sortingOrderArray[index] > command.sortingOrder)
+				{
+					continue;
+				}
+
+				// 데이터 기록.
+				frame->charInfoArray[index].Char.AsciiChar = command.text[sourceIndex];
+				frame->charInfoArray[index].Attributes = (WORD)command.color;
+
+				// 우선 순위 업데이트
+				frame->sortingOrderArray[index] = command.sortingOrder;
+			}
+		}
+
+		// 그리기.
+		GetCurrentBuffer()->Draw(frame->charInfoArray);
+
+		// 버퍼 교환.
+		Present();
+
+		// 렌더 큐 비우기.
+		renderQueue.clear();
 	}
 	Renderer& Renderer::Get()
 	{
@@ -94,18 +170,36 @@ namespace Wanted
 	}
 	void Renderer::Clear()
 	{
-
+		// 화면 지우기.
+		// 1. 프레임(2차원 배열 데이터) 지우기.
+		frame->Clear(screenSize);
+		
+		// 2. 콘솔 버퍼 지우기.
+		GetCurrentBuffer()->Clear();
 	}
 	void Renderer::Submit(const char* text, const Vector2& position, Color color, int sortingOrder)
 	{
+		// 렌더 데이터 생성 후 큐에 추가.
+		RenderCommand command = {};
+		command.text = text;
+		command.position = position;
+		command.color = color;
+		command.sortingOrder = sortingOrder;
+		
+		renderQueue.emplace_back(command);
 	}
 
 	void Renderer::Present()
 	{
+		// 버퍼 교환.
+		SetConsoleActiveScreenBuffer(GetCurrentBuffer()->GetBuffer());
+		
+		// 인덱스 쿄체.
+		currentBufferIndex = 1 - currentBufferIndex;
 	}
 
 	ScreenBuffer* Renderer::GetCurrentBuffer()
 	{
-		return nullptr;
+		return screenBuffers[currentBufferIndex];
 	}
 }
