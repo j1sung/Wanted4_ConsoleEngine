0fb65dcefd36dbea924c0987a9e2342593949389
dalssak <dalssagi00@gmail.com>
2026-02-03 15:53:51 +0900

    이중 버퍼링 기능 추가 완료
    - Draw 처리 로직 완성

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -11,48 +11,48 @@ using namespace Wanted;
 
 int main()
 {
-	// 예시 배열.
-	const int width = 3;
-	const int height = 3;
-	CHAR_INFO charArray[width * height] = {};
+	//// 예시 배열.
+	//const int width = 3;
+	//const int height = 3;
+	//CHAR_INFO charArray[width * height] = {};
 
-	charArray[2 * width + 1].Char.AsciiChar = 'P';
-	charArray[2 * width + 1].Attributes = FOREGROUND_GREEN;
+	//charArray[2 * width + 1].Char.AsciiChar = 'P';
+	//charArray[2 * width + 1].Attributes = FOREGROUND_GREEN;
 
-	charArray[0 * width + 1].Char.AsciiChar = '#';
-	charArray[0 * width + 1].Attributes = FOREGROUND_RED;
+	//charArray[0 * width + 1].Char.AsciiChar = '#';
+	//charArray[0 * width + 1].Attributes = FOREGROUND_RED;
 
-	COORD position = {};
-	COORD screenSize;
-	screenSize.X = width;
-	screenSize.Y = height;
+	//COORD position = {};
+	//COORD screenSize;
+	//screenSize.X = width;
+	//screenSize.Y = height;
 
-	SMALL_RECT writeRegion{
-		0,
-		0,
-		static_cast<short>(width),
-		static_cast<short>(height)
-	};
+	//SMALL_RECT writeRegion{
+	//	0,
+	//	0,
+	//	static_cast<short>(width),
+	//	static_cast<short>(height)
+	//};
 
-	WriteConsoleOutputA(
-		GetStdHandle(STD_OUTPUT_HANDLE),
-		charArray,
-		screenSize,
-		position,
-		&writeRegion
-	);
+	//WriteConsoleOutputA(
+	//	GetStdHandle(STD_OUTPUT_HANDLE),
+	//	charArray,
+	//	screenSize,
+	//	position,
+	//	&writeRegion
+	//);
 
 	//CreateConsoleScreenBuffer(); // 화면에 보이게 전환
 	//SetConsoleActiveScreenBuffer();
 	
-
+	// ----------------------- 위에 콘솔 이중 버퍼 그리기 -----------------------------
 
 	//Wanted::Engine engine;
 	//engine.SetNewLevel(new SokobanLevel());
 	//engine.Run();
 
-	/*Game game;
-	game.Run();*/
+	Game game;
+	game.Run();
 
 	// 문자 배열을 한번에 콘솔로 넘겨서 그릴 때 사용하는 함수.
 	//WriteConsoleOutputA();
